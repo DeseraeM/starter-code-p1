4a5e3eab055c9e5b4c120c5a0c19cf01f43c12e9
Deserae <deserae@ucsb.edu>
2026-04-18 21:05:48 -0700

    testing

---
+++ pop3_read.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+
+# Include needed libraries. Do _not_ include any libraries not included with
+# Python3 (i.e. do not use `pip`).
+import socket
+import sys
+
+BUFFER_SIZE = 4096
+PASSWORD = 'password'
+
+# Parse command-line arguments.
+host     = sys.argv[1]
+port     = int(sys.argv[2])
+username = sys.argv[3]
+
+# Establish a TCP connection with the POP3 server.
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((host, port))
+
+# Read greeting from the server.
+data = s.recv(BUFFER_SIZE)
+response = data.decode('utf-8')
+
+if not response.startswith('+OK'):
+    raise Exception('+OK not received from server.')
+
+# Note: the POP3 spec requires every command to end with \r\n, not just \n.
+
+# Log in with USER and PASS commands.
+s.send(f"USER {username} \r\n."encode())
+data = s.recv(BUFFER_SIZE)
+response = data.decode('utf-8')
+
+if not response.startswith('+OK'):
+    raise Exception('+OK not received from server.')
+
+
+
+s.send(f"PASS {PASSWORD} \r\n" .encode())
+data = s.recv(BUFFER_SIZE)
+response = data.decode('utf-8')
+
+if not response.startswith('+OK'):
+    raise Exception('+OK not received from server.')
+
+
+# Get the number of messages with the LIST command.
+# Note: the LIST response spans multiple lines and ends with a line
+# containing only '.'. Do not assume a fixed number of recv() calls will
+# capture the full response — depending on the network, the entire response
+# may arrive in a single recv() or be split across several. Accumulate data
+# until you have seen the terminator.
+s.send('LIST \r\n'.encode())
+data = s.recv(BUFFER_SIZE)
+response = data.decode('utf-8')
+
+if not response.startswith('+OK'):
+    raise Exception('+OK not received from server.')
+ldata = []
+response = '' 
+while not response.startswith('.'):
+    ldata.append(s.recv(BUFFER_SIZE))
+    last = ldata[-1]
+    response = last.decode('utf-8')
+
+
+
+
+# Retrieve and print each message with the RETR command.
+# The same caveat about multi-line responses applies here.
+# Print messages separated by a line containing only '---'.
+for d in ldata:
+    if d.decode() == '.':
+        continue
+    newdata = d.decode('utf-8')
+    s.send(f"RETR {newdata.split()[0]} \r\n".encode())
+    ndata = s.recv(BUFFER_SIZE)
+    response = '' 
+    while not response.startswith('.'):
+        data = s.recv(BUFFER_SIZE)
+        response = data.decode('utf-8')
+    print(data.decode())
+    print("---")
+s.send('QUIT \r\n'.encode())
+
+
+# Close the socket when finished.
+s.close()
+
+for d in ldata:
+    if d.encode() == '.':
+        continue
+    else: newdata = d.decode('utf-8')
---
+++ smtp_send.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+
+# Include needed libraries. Do _not_ include any libraries not included with
+# Python3 (i.e. do not use `pip`).
+import socket
+import sys
+
+BUFFER_SIZE = 4096
+
+# Parse command-line arguments.
+host    = sys.argv[1]
+port    = int(sys.argv[2])
+from_addr = sys.argv[3]
+to_addr   = sys.argv[4]
+subject   = sys.argv[5]
+
+# Read the email body from standard input.
+body = sys.stdin.read()
+
+# Establish a TCP connection with the SMTP server.
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((host, port))
+
+
+# Read greeting from the server.
+data = s.recv(BUFFER_SIZE)
+response = data.decode('utf-8')
+
+if not response.startswith('220'):
+    raise Exception('220 reply not received from server.')
+
+# Note: the SMTP spec requires every command to end with \r\n, not just \n.
+# This applies to commands (HELO, MAIL FROM, etc.) and to the message headers
+# and body sent after DATA.
+
+# Send HELO command and get server response.
+s.send('HELO client\r\n'.encode())
+response = s.recv(BUFFER_SIZE).decode('utf-8')
+if not response.startswith('250'):
+    raise Exception('250 reply not received from server.')
+
+# Send MAIL FROM command.
+s.send('MAIL FROM: < '.encode() + from_addr.encode() + '>\r\n'.encode())
+response = s.recv(BUFFER_SIZE).decode()
+if not response.startswith('250'):
+    raise Exception('250 reply not received from server.')
+
+
+
+# Send RCPT TO command.
+s.send ('RCPT TO: < '.encode() + to_addr.encode() + '>\r\n'.encode())
+response = s.recv(BUFFER_SIZE).decode()
+if not response.startswith('250'):
+    raise Exception('250 reply not received from server.')
+
+
+# Send DATA command.
+s.send ('DATA client\r\n'.encode())
+response = s.recv(BUFFER_SIZE).decode()
+if not response.startswith('250'):
+    raise Exception('250 reply not received from server.')
+
+# Send message headers and body.
+s.send('Subject:'.encode() + Subject.encode() '\r\n')
+
+
+# End message with a line containing only a period.
+s.send('. \r\n'.encode)
+
+# Send QUIT command.
+s.send ('QUIT \r\n'.encode()) #might not need the .encode() 
+
+
+# Close the socket when finished.
+s.close()
